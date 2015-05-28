87b8310860b6749dc97f5f8008dc71af3ed94cf2
Michel Meyer <micmeyer@uos.de>
2015-05-28 06:05:58 +0200

    Using an ArrayList instead of a LinkedList solved Problem 23.

--- problem0023/Problem0023.java
+++ problem0023/Problem0023.java
@@ -1,8 +1,9 @@
 package problem0023;
 
+import java.util.ArrayList;
 import java.util.Collections;
-import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import util.Divisors;
 
@@ -13,13 +14,13 @@ import util.Divisors;
 public class Problem0023
 {
 
-    private static final List<Integer> abundants = new LinkedList<>();
-    
+    private static final List<Integer> abundants = new ArrayList<>();
+
     public static boolean isAbundant(int n)
     {
         return Divisors.getAllProperDivisors(n).stream().mapToLong(Long::longValue).sum() > n;
     }
-    
+
     public static boolean isSumOfTwoAbundants(int n)
     {
         boolean isSum = false;
@@ -36,32 +37,33 @@ public class Problem0023
 //        System.out.printf("%5d : %b\n", n, isSum);
         return isSum;
     }
-    
+
     private static boolean abundantsContains(int n)
     {
         return Collections.binarySearch(abundants, n) >= 0;
     }
-    
+
     public static void main(String[] args)
     {
-        IntStream.range(1, 28123).filter(Problem0023::isAbundant).forEach(abundants::add);
-        
-        
-        long sum = 0;
         long time = System.currentTimeMillis();
-        for (int t = 1; t < 28124; t++)
+        
+        IntStream.range(1, 28123).filter(Problem0023::isAbundant).forEach(abundants::add);
+
+        long sum = 0;
+        for (int t = 1; t < 28130; t++)
         {
-            if (isSumOfTwoAbundants(t))
+            if (!isSumOfTwoAbundants(t))
             {
                 sum += t;
             }
             if (t % 100 == 0)
             {
-                System.out.println(t + " Time: " + (System.currentTimeMillis() - time));
                 time = System.currentTimeMillis();
             }
         }
-        
+
         System.out.println(sum);
+
+        System.out.println("Time: " + (System.currentTimeMillis() - time));
     }
 }
