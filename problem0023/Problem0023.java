7b9e417951e55fb141a65a9cd74768491ea7e884
Michel Meyer <micmeyer@uos.de>
2015-05-28 06:29:03 +0200

    Finished Problem 23 and solved Problem 24.

--- problem0023/Problem0023.java
+++ problem0023/Problem0023.java
@@ -9,6 +9,24 @@ import util.Divisors;
 
 /**
  *
+ * A perfect number is a number for which the sum of its proper divisors is
+ * exactly equal to the number. For example, the sum of the proper divisors of
+ * 28 would be 1 + 2 + 4 + 7 + 14 = 28, which means that 28 is a perfect number.
+ * <br/><br/>
+ * A number n is called deficient if the sum of its proper divisors is less than
+ * n and it is called abundant if this sum exceeds n.<br/>
+ * <br/>
+ * As 12 is the smallest abundant number, 1 + 2 + 3 + 4 + 6 = 16, the smallest
+ * number that can be written as the sum of two abundant numbers is 24. By
+ * mathematical analysis, it can be shown that all integers greater than 28123
+ * can be written as the sum of two abundant numbers. However, this upper limit
+ * cannot be reduced any further by analysis even though it is known that the
+ * greatest number that cannot be expressed as the sum of two abundant numbers
+ * is less than this limit.<br/>
+ * <br/>
+ * Find the sum of all the positive integers which cannot be written as the sum
+ * of two abundant numbers.
+ *
  * @author micmeyer
  */
 public class Problem0023
@@ -24,11 +42,11 @@ public class Problem0023
     public static boolean isSumOfTwoAbundants(int n)
     {
         boolean isSum = false;
-        int item = 0;
-        for (int t = 0; t < abundants.size() && item < n && !isSum; t++)
+        int summand = 0;
+        for (int t = 0; t < abundants.size() && summand < n && !isSum; t++)
         {
-            item = abundants.get(t);
-            int diff = n - item;
+            summand = abundants.get(t);
+            int diff = n - summand;
             if (abundantsContains(diff))
             {
                 isSum = true;
@@ -46,19 +64,15 @@ public class Problem0023
     public static void main(String[] args)
     {
         long time = System.currentTimeMillis();
-        
+
         IntStream.range(1, 28123).filter(Problem0023::isAbundant).forEach(abundants::add);
 
         long sum = 0;
-        for (int t = 1; t < 28130; t++)
+        for (int t = 1; t < 28124; t++)
         {
             if (!isSumOfTwoAbundants(t))
             {
                 sum += t;
-            }
-            if (t % 100 == 0)
-            {
-                time = System.currentTimeMillis();
             }
         }
 
