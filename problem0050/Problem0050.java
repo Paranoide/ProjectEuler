a2b914b91509fc3431225b4db6c34365a3ec2638
Michel Meyer <micmeyer@uos.de>
2015-06-22 21:43:10 +0200

    First brute force attempt, way too slow

--- problem0050/Problem0050.java
+++ problem0050/Problem0050.java
@@ -1,22 +1,63 @@
 package problem0050;
 
+import java.util.Collections;
+import java.util.List;
+import util.PrimeGenerator;
+
 /**
  *
- * 
- * 
+ *
+ *
  * @author micmeyer
  */
 public class Problem0050
 {
-   
+
     public static void main(String[] args)
     {
         long time = System.currentTimeMillis();
-        
-        
-        
+
+        List<Long> primes = new PrimeGenerator().generatePrimesSmallerThanN(1000000);
+
+        System.out.println(primes.size());
+        System.out.println((primes.size() * (primes.size() + 1)) / 2);
+
+        long maxPrime = 0;
+        int maxCount = 0;
+        for (int firstIndex = primes.size() - 2; firstIndex >= 0; firstIndex--)
+        {
+            long sum = 0;
+            for (int lastIndex = firstIndex - 1; lastIndex >= 0 && sum < 1000000; lastIndex--)
+            {
+                for (int pIndex = firstIndex; pIndex >= lastIndex; pIndex--)
+                {
+                    sum += primes.get(lastIndex);
+                }
+
+                int count = lastIndex - firstIndex + 1;
+                if (contains(primes, sum) && count > maxCount)
+                {
+                    maxPrime = sum;
+                    maxCount = count;
+                    System.out.println(maxPrime);
+                    System.out.println(maxCount);
+                }
+                sum = 0;
+            }
+
+            System.out.println(firstIndex);
+
+        }
+
+        System.out.println("Max: " + maxPrime);
+
         System.out.println();
-        System.out.println("Time: " + (System.currentTimeMillis()-time));
+        System.out.println("Time: " + (System.currentTimeMillis() - time));
     }
-    
+
+    private static boolean contains(List<Long> list, Long item)
+    {
+        return Collections.binarySearch(list, item) >= 0;
+    }
+
 }
