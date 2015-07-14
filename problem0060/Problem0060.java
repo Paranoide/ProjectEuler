14df91526a1734bde70ffe776b288147dc403286
Michel Meyer <micmeyer@uos.de>
2015-07-14 20:25:41 +0200

    Added problem description

--- problem0060/Problem0060.java
+++ problem0060/Problem0060.java
@@ -2,21 +2,26 @@ package problem0060;
 
 /**
  *
- * 
- * 
+ * The primes 3, 7, 109, and 673, are quite remarkable. By taking any two primes
+ * and concatenating them in any order the result will always be prime. For
+ * example, taking 7 and 109, both 7109 and 1097 are prime. The sum of these
+ * four primes, 792, represents the lowest sum for a set of four primes with
+ * this property.<br/>
+ * <br/>
+ * Find the lowest sum for a set of five primes for which any two primes
+ * concatenate to produce another prime.
+ *
  * @author micmeyer
  */
 public class Problem0060
 {
-   
+
     public static void main(String[] args)
     {
         long time = System.currentTimeMillis();
-        
-        
-        
+
         System.out.println();
-        System.out.println("Time: " + (System.currentTimeMillis()-time));
+        System.out.println("Time: " + (System.currentTimeMillis() - time));
     }
-    
+
 }
