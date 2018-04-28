fd1929fd128ac3220c6a3a5723f57c7e2ceb01c1
Michel Meyer <micmeyer@uos.de>
2018-04-28 12:04:37 +0200

    Added a Sets util class.

---
+++ util/Sets.java
@@ -0,0 +1,66 @@
+package util;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ *
+ * @author Michel Meyer <micmeyer@uos.de>
+ */
+public class Sets
+{
+    private static int counter = 0;
+
+    public static <T> List<Set<T>> findAllSubsetsOfLengthN(Collection<T> elements, int n)
+    {
+        int size = elements.size();
+
+        if (size < n)
+            return new ArrayList<>();
+
+        List<Set<T>> allSubsets = new ArrayList<>();
+        ArrayList<T> elementsAsArrayList = new ArrayList<>(elements);
+
+        findAllSubsetsOfLengthNRec(elementsAsArrayList, n, allSubsets, new int[n], 1, 0, size);
+
+        return allSubsets;
+    }
+
+    private static <T> void findAllSubsetsOfLengthNRec(ArrayList<T> elements, int n, List<Set<T>> subsets,
+            int[] indices, int depth, int firstIndex, int size)
+    {
+        if (depth > n)
+        {
+            Set<T> subset = new HashSet<>();
+            for (int index : indices)
+                subset.add(elements.get(index));
+            subsets.add(subset);
+            counter++;
+
+            return;
+        }
+
+        int lastIndex = size - n + depth;
+
+        for (int t = firstIndex; t < lastIndex; t++)
+        {
+            indices[depth - 1] = t;
+            findAllSubsetsOfLengthNRec(elements, n, subsets, indices, depth + 1, t + 1, size);
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        List<Integer> list = new ArrayList<>();
+        for (int t = 0; t < 10; t++)
+            list.add(t);
+
+        List<Set<Integer>> allSubsets = findAllSubsetsOfLengthN(list, 6);
+
+        for (Set<Integer> subset : allSubsets)
+            System.out.println(subset);
+    }
+}
