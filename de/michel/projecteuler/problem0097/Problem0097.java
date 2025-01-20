a363577fd68b23c44b8771e247fd388b281b125c
Michel Meyer <micmeyer@uos.de>
2025-01-20 19:56:31 +0100

    Solved Problem 97.

--- de/michel/projecteuler/problem0097/Problem0097.java
+++ de/michel/projecteuler/problem0097/Problem0097.java
@@ -1,22 +1,29 @@
 package de.michel.projecteuler.problem0097;
 
+import java.math.BigInteger;
+
 /**
- *
- * 
- * 
  * @author micmeyer
  */
 public class Problem0097
 {
-   
+
     public static void main(String[] args)
     {
         long time = System.currentTimeMillis();
-        
-        
-        
+
+        BigInteger a = BigInteger.valueOf(28433);
+        BigInteger p = BigInteger.valueOf(7_830_457);
+        BigInteger mod = BigInteger.valueOf(10_000_000_000L);
+
+        BigInteger lastSomeDigitsOfPrime = BigInteger.TWO.modPow(p, mod).multiply(a).add(BigInteger.ONE);
+
+        BigInteger lastTenDigits = lastSomeDigitsOfPrime.mod(mod);
+
+        System.out.println("Result: " + lastTenDigits);
+
         System.out.println();
-        System.out.println("Time: " + (System.currentTimeMillis()-time));
+        System.out.println("Time: " + (System.currentTimeMillis() - time));
     }
-    
+
 }
