d19e2eff072a9bc8af55397b41203271b0fe6c42
Michel Meyer <micmeyer@uos.de>
2025-01-20 19:55:59 +0100

    Solved Problem 96.

--- de/michel/projecteuler/problem0096/Problem0096.java
+++ de/michel/projecteuler/problem0096/Problem0096.java
@@ -1,22 +1,54 @@
 package de.michel.projecteuler.problem0096;
 
+import java.io.File;
+import java.net.URL;
+import java.util.List;
+
 /**
- *
- * 
- * 
  * @author micmeyer
  */
 public class Problem0096
 {
-   
+
     public static void main(String[] args)
     {
         long time = System.currentTimeMillis();
-        
-        
-        
+
+        File sudokuFile = getSudokuFile("p096_sudoku.txt");
+
+        List<Sudoku> sudokus = SudokuLoader.loadFrom(sudokuFile);
+
+        int result = 0;
+
+        for (Sudoku sudoku : sudokus)
+        {
+            SudokuSolver solver = new SudokuSolver(sudoku);
+
+            boolean success = solver.solve();
+            if (!success)
+                throw new RuntimeException("No success!");
+
+            result += getFirst3DigitsAsNumber(sudoku);
+        }
+
+        System.out.println("Result: " + result);
         System.out.println();
-        System.out.println("Time: " + (System.currentTimeMillis()-time));
+        System.out.println("Time: " + (System.currentTimeMillis() - time));
     }
-    
+
+    private static File getSudokuFile(String fileName)
+    {
+        URL resource = Problem0096.class.getResource(fileName);
+        if (resource == null)
+            throw new IllegalArgumentException("File " + fileName + " not found.");
+
+        return new File(resource.getFile());
+    }
+
+    private static int getFirst3DigitsAsNumber(Sudoku sudoku)
+    {
+        return sudoku.get(0, 0) * 100
+                + sudoku.get(0, 1) * 10
+                + sudoku.get(0, 2);
+    }
 }
---
+++ de/michel/projecteuler/problem0096/SudokuSolver.java
@@ -0,0 +1,53 @@
+package de.michel.projecteuler.problem0096;
+
+public class SudokuSolver
+{
+    private final Sudoku sudoku;
+
+    public SudokuSolver(Sudoku sudoku)
+    {
+        this.sudoku = sudoku;
+    }
+
+    public boolean solve()
+    {
+        return this.solveRec(0,0);
+    }
+
+    private boolean next(int row, int col)
+    {
+        int nextRow = row;
+        int nextCol = col+1;
+        if (nextCol >= 9)
+        {
+            nextRow++;
+            nextCol = 0;
+        }
+        return this.solveRec(nextRow, nextCol);
+    }
+
+    private boolean solveRec(int row, int col)
+    {
+        if (row >= 9 || col >= 9)
+            return true;
+
+        if (!this.sudoku.isFree(row, col))
+            return next(row, col);
+
+        for (int digit = 1; digit <= 9; digit++)
+        {
+            if (this.sudoku.canBePlaced(row, col, digit))
+            {
+                this.sudoku.set(row, col, digit);
+
+                boolean success = this.next(row, col);
+                if (success)
+                    return true;
+
+                this.sudoku.remove(row, col);
+            }
+        }
+
+        return false;
+    }
+}
